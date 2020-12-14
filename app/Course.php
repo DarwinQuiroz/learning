2e7ffaf40cd8ccd2e382f8ada660263a9b5e1374
darwin_quiroz <darquiroz@gmail.com>
2020-12-13 19:45:56 -0500

    Configuraciones adicionales

--- app/Course.php
+++ app/Course.php
@@ -5,6 +5,45 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * App\Course
+ *
+ * @property int $id
+ * @property int $teacher_id
+ * @property int $category_id
+ * @property int $level_id
+ * @property string $name
+ * @property string $description
+ * @property string $slug
+ * @property string|null $picture
+ * @property string $status
+ * @property int $previous_approved
+ * @property int $previous_rejected
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Course newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Course newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Course onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Course query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereLevelId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course wherePicture($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course wherePreviousApproved($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course wherePreviousRejected($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereTeacherId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Course whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Course withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Course withoutTrashed()
+ * @mixin \Eloquent
+ */
 class Course extends Model
 {
     use SoftDeletes;
